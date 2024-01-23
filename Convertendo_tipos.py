c580cd78488bb269579d3f6a22626dd2717efb48
igorncosta <engigornunes@outlook.com>
2024-01-23 15:35:18 -0400

    ok12 ok

---
+++ Convertendo_tipos.py
@@ -0,0 +1,12 @@
+print(int(5.9))
+print(str("10"))
+print(float("10.10"))
+print(float(100))
+print(type(str(10.10)))
+
+print(10/10)
+print(10//10)
+
+valor =  10
+valor_str = str(valor)
+print(valor, valor_str)
---
+++ variaveis_constantes.py
@@ -0,0 +1,10 @@
+nome = 'Igor'
+idade = 30
+
+
+limite_saque_diario = 500
+print('Olá', nome,'.','Seu limite diário é', limite_saque_diario)
+BR_ESTATOS = ["SP","MS","SC"]
+
+print(BR_ESTATOS)
+
