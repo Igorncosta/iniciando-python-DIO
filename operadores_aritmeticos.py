3350c00853bbb5399c847b7ffa2d48701e47a63e
igorncosta <engigornunes@outlook.com>
2024-01-25 16:51:58 -0400

    ok

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,12 @@
+produto_1 = 30
+produto_2 = 20
+
+adicao = (produto_1 + produto_2 + 3.5)
+print("Adição= ", adicao)
+subt = (produto_1 - produto_2 - 3.5)
+print("Subtração= ", subt)
+mult = (produto_1 * produto_2 * 3.5)
+print("Multiplicação= ", mult)
+divisao = (produto_1 / produto_2)
+divisao_int = (produto_1 //  produto_2)
+print("Divisão= ", divisao, divisao_int)
